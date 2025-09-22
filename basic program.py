76daacd7b34dea71bdad17fed980adb41b8174b7
SindhujaBommineni <busireddysindhu494@gmail.com>
2025-09-22 15:12:18 +0300

    Add files via upload

---
+++ basic program.py
@@ -0,0 +1,17 @@
+"""print("hello world")
+a=max(30,50)
+print(a)
+m=min(-2,-5)
+print(m)
+#single comment
+ multiple lines comment
+#variables -assign /storing values in variables
+#varriables is a case sensitive -have some rules
+#memory will create when assigning values
+#latest assigned value will be considered """
+
+""" c="sindhu"
+c="aadvik"
+c="anvesh"
+print(c) """
+#python is a interepreter language means it will complie line by line
